56ded888045d2e0b56772dad80b46d7e1fb21209
victoradauto1 <46942765+victoradauto1@users.noreply.github.com>
2023-03-31 18:05:32 -0300

    continuação de fundamentos

--- 03-AVANÇANDO/index.js
+++ 03-AVANÇANDO/index.js
@@ -3,3 +3,27 @@
 let numbers = [1, 2, 3];
 numbers.push(5);
 console.log(numbers[2]);
+// outra sintaxe de array
+let number = [2, 6, 87, 192];
+number.push(304);
+console.log(number);
+// tipo any - aceita qualquer tipo de valor
+const arr1 = [1, "teste", "carro", 235, true, [], { "chave": "valor" }];
+console.log(arr1);
+// 4- parametros tipados
+function soma(a, b) {
+    return (a + b);
+}
+let resul = soma(32, 87);
+console.log(resul);
+// retorno de função
+function gretting(name) {
+    return `Olá, ${name}`;
+}
+console.log(gretting("Victor"));
+// função anônima
+setTimeout(function () {
+    const salary = 1000;
+    //console.log(parseFloat(salary))
+}, 2000);
+// tipos de obejto
--- 03-AVANÇANDO/index.ts
+++ 03-AVANÇANDO/index.ts
@@ -4,3 +4,47 @@ let numbers: number[] = [1,2,3]
 numbers.push(5)
 
 console.log(numbers[2])
+
+// outra sintaxe de array
+
+let number: Array<number> = [2, 6, 87, 192]
+
+number.push(304)
+
+console.log(number)
+
+// tipo any - aceita qualquer tipo de valor
+
+const arr1: any = [1, "teste", "carro", 235, true, [], {"chave": "valor"}]
+
+console.log(arr1)
+
+// 4- parametros tipados
+
+function soma (a:number, b:number){
+    return (a+b)
+}
+
+let resul = soma(32,87)
+
+console.log(resul)
+
+// retorno de função
+ function gretting (name: string): string{
+    return `Olá, ${name}`
+ }
+
+ console.log(gretting("Victor"))
+
+ // função anônima
+
+ setTimeout(function(){
+
+        const salary:number = 1000
+
+        //console.log(parseFloat(salary))
+ }, 2000)
+
+
+// tipos de obejto
+
