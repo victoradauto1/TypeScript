631157cb3d038ad8600939b8eb087f08bb0403f6
victoradauto1 <46942765+victoradauto1@users.noreply.github.com>
2023-04-06 17:59:41 -0300

    generic functions

--- 05-FUNCOES/index.js
+++ 05-FUNCOES/index.js
@@ -14,3 +14,29 @@ function PreGreeting(f, userName) {
     console.log(greet);
 }
 PreGreeting(greeting, "Matheus");
+// 3 - generic functions
+function firstElement(arr) {
+    return arr[0];
+}
+console.log(firstElement([1, 2, 3, 4, 5]));
+console.log(firstElement(["A", "B", "C", "D"]));
+function mergeObjects(obj1, obj2) {
+    return Object.assign(Object.assign({}, obj1), obj2);
+}
+const newObject = mergeObjects({ "name": "Matheus" }, { "age": 30, job: "Programmer" });
+console.log(newObject);
+// 4- contraints
+function biggestNumber(a, b) {
+    let biggest;
+    if (+a > +b) {
+        biggest = a;
+    }
+    else {
+        biggest = b;
+    }
+    return biggest;
+}
+biggestNumber(2, 3);
+biggestNumber(459, 34);
+console.log(biggestNumber(2, 3));
+console.log(biggestNumber(459, 34));
--- 05-FUNCOES/index.ts
+++ 05-FUNCOES/index.ts
@@ -22,3 +22,41 @@ function PreGreeting(f: (name:string)=> string,  userName:string){
 }
 
 PreGreeting(greeting, "Matheus")
+
+// 3 - generic functions
+
+function firstElement<T>(arr: T[]): T{
+    return arr[0]
+}
+
+console.log(firstElement([1, 2, 3, 4, 5]))
+console.log(firstElement(["A", "B", "C", "D"]))
+
+function mergeObjects <U, T> (obj1:U, obj2: T) {
+    return {...obj1, ...obj2} 
+    
+}
+
+const newObject = mergeObjects({"name":"Matheus"}, {"age": 30, job: "Programmer"})
+console.log(newObject)
+
+// 4- contraints
+
+function biggestNumber < T extends number | string> (a: T, b: T ): T {
+    
+    let biggest: T
+
+    if(+a > +b){
+        biggest = a
+    } else {
+        biggest = b
+    }
+
+    return biggest
+}
+
+biggestNumber(2, 3)
+biggestNumber(459, 34)
+
+console.log(biggestNumber(2, 3))
+console.log(biggestNumber(459, 34))
