afbf72274a79dced4c4ab642606ca0daedfa7c9f
Victor Adauto <victoradauto@yahoo.com.br>
2023-04-17 18:08:03 -0300

    itens e métodos públicos, protegidos e privados

--- 08-CLASSES EM TYPESCRIPT/index.js
+++ 08-CLASSES EM TYPESCRIPT/index.js
@@ -69,3 +69,136 @@ class Truck {
 const fuerza = new Truck("Super", 2500);
 console.log(fuerza);
 fuerza.showDeatils();
+// 7- getters
+class Person {
+    constructor(name, surname) {
+        this.name = name;
+        this.surname = surname;
+    }
+    get fullName() {
+        return `${this.name} ${this.surname}`;
+    }
+}
+const mathesuBattisti = new Person("Matheus", "Battisti");
+console.log(mathesuBattisti.name);
+console.log(mathesuBattisti.surname);
+console.log(mathesuBattisti.fullName);
+//8- setters
+class Coord {
+    set fillX(x) {
+        if (x == 0) {
+            return;
+        }
+        else {
+            this.x = x;
+            console.log(" 'x inserido com sucesso!");
+        }
+    }
+    set fillY(y) {
+        if (y === 0) {
+            return;
+        }
+        else {
+            this.y = y;
+            console.log(" 'y inserido com sucesso");
+        }
+    }
+    get getCoords() {
+        return console.log(`Os valores são ${this.x} e ${this.y}`);
+    }
+}
+const myCoords = new Coord;
+myCoords.fillX = 15;
+myCoords.fillY = 0;
+console.log(myCoords);
+myCoords.fillY = 302;
+console.log(myCoords);
+myCoords.getCoords;
+class BlogPost {
+    constructor(title) {
+        this.title = title;
+    }
+    itemTitle() {
+        return `O título do post é ${this.title}`;
+    }
+}
+const myPost = new BlogPost("Hello World");
+console.log(myPost.itemTitle());
+class TestingInterface {
+    constructor(title) {
+        this.title = title;
+    }
+    itemTitle() {
+        return `O título remanescente ficou ${this.title}`;
+    }
+}
+const novoPost = new TestingInterface("Amanhã");
+console.log(novoPost.itemTitle());
+//10- override de métodos
+class Base {
+    someMethod() {
+        console.log("alguma coisa");
+    }
+}
+class Nova extends Base {
+    someMethod() {
+        console.log("testando outra coisa");
+    }
+}
+const myObject = new Nova;
+myObject.someMethod();
+// 11-public
+class C {
+    constructor() {
+        this.x = 10;
+    }
+}
+const cinstance = new C;
+console.log(cinstance.x);
+class D extends C {
+}
+const distance = new D;
+console.log(distance.x);
+//12- protected
+class E {
+    constructor() {
+        this.x = 15;
+    }
+    protectedMethod() {
+        console.log `Este método é protegido`;
+    }
+}
+class F extends E {
+    showX() {
+        console.log(`x: ${this.x}`);
+    }
+    showProtectedMethod() {
+        this.protectedMethod();
+    }
+}
+const finstance = new F;
+// console.log(finstance.x)
+finstance.showX();
+// finstance.protectedMethod()
+finstance.showProtectedMethod();
+//13- private
+class PrivateClass {
+    constructor() {
+        this.name = "Private";
+    }
+    showName() {
+        console.log(`${this.name}`);
+    }
+    privateMethod() {
+        console.log("Este método é privado");
+    }
+    showPrivateMethod() {
+        this.privateMethod();
+    }
+}
+const pObj = new PrivateClass;
+// console.log(pObj.name)
+pObj.showName();
+// class TestingPrivate extends PrivateClass{
+// }
+pObj.showPrivateMethod();
--- 08-CLASSES EM TYPESCRIPT/index.ts
+++ 08-CLASSES EM TYPESCRIPT/index.ts
@@ -118,3 +118,209 @@ const fuerza = new Truck("Super", 2500)
 console.log(fuerza)
 
 fuerza.showDeatils()
+
+// 7- getters
+
+class Person{
+    name
+    surname
+
+    constructor (name: string, surname: string){
+        this.name = name
+        this.surname = surname
+    }
+
+    get fullName(){
+        return `${this.name} ${this.surname}`
+    }
+}
+
+const mathesuBattisti =  new Person("Matheus", "Battisti")
+
+console.log(mathesuBattisti.name)
+console.log(mathesuBattisti.surname)
+console.log(mathesuBattisti.fullName)
+
+//8- setters
+
+class Coord {
+
+    x!: number
+    y!: number
+
+    set fillX(x: number){
+        if(x == 0){
+            return
+        } else {
+            this.x = x
+
+            console.log(" 'x inserido com sucesso!")
+        }
+    }
+
+    set fillY(y: number){
+        if(y === 0){
+            return
+        } else {
+            this.y = y
+            
+            console.log(" 'y inserido com sucesso")
+        }
+    }
+
+    get getCoords(){
+        return console.log(`Os valores são ${this.x} e ${this.y}`)
+    }
+}
+
+const myCoords = new Coord
+
+myCoords.fillX = 15
+myCoords.fillY = 0
+
+console.log(myCoords)
+
+myCoords.fillY = 302
+
+console.log(myCoords)
+
+myCoords.getCoords
+
+// 9- implements
+
+interface showTitle{
+    itemTitle():string
+}
+
+class BlogPost implements showTitle{
+
+    title
+
+    constructor (title: string){
+        this.title = title
+    }
+
+    itemTitle() {
+        return `O título do post é ${this.title}`
+        
+    }
+
+}
+
+const myPost = new BlogPost("Hello World")
+
+console.log(myPost.itemTitle())
+
+class TestingInterface implements showTitle{
+    
+    title
+
+    constructor (title: string){
+        this.title = title
+    }
+
+    itemTitle() {
+        return `O título remanescente ficou ${this.title}`
+        
+    }
+}
+
+const novoPost = new TestingInterface("Amanhã")
+
+console.log(novoPost.itemTitle())
+
+//10- override de métodos
+
+class Base{
+    someMethod(){
+        console.log("alguma coisa")
+    }
+}
+
+class Nova extends Base{
+    someMethod(){
+        console.log("testando outra coisa")
+    }
+}
+
+const myObject = new Nova
+
+myObject.someMethod()
+
+// 11-public
+
+class C {
+    public x = 10
+}
+
+const cinstance = new C
+
+console.log(cinstance.x)
+
+class D extends C {
+
+}
+
+const distance = new D
+
+console.log(distance.x)
+
+//12- protected
+
+class E {
+    protected x =15
+
+    protected protectedMethod(){
+        console.log `Este método é protegido`
+    }
+}
+
+class F extends E {
+
+    showX (){
+        console.log(`x: ${this.x}`)
+    }
+
+    showProtectedMethod(){
+        this.protectedMethod()
+    }
+}
+
+const finstance = new F
+
+// console.log(finstance.x)
+finstance.showX()
+
+// finstance.protectedMethod()
+finstance.showProtectedMethod()
+
+//13- private
+
+class PrivateClass{
+    private name = "Private"
+
+    showName(){
+        console.log(`${this.name}`)
+    }
+
+    private privateMethod(){
+        console.log("Este método é privado")
+    }
+
+    showPrivateMethod(){
+        this.privateMethod()
+    }
+
+
+}
+
+const pObj = new PrivateClass
+
+// console.log(pObj.name)
+pObj.showName()
+
+// class TestingPrivate extends PrivateClass{
+    
+// }
+
+pObj.showPrivateMethod()
